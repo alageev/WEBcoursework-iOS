81bb3e27625e1fb29f18ea4c182e53430a896f44
alageev <alageev01@gmail.com>
2020-10-21 23:37:39 +0300

    Initial Commit

---
+++ Shared/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  Shared
+//
+//  Created by Алексей Агеев on 21.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
